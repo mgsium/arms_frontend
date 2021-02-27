83ef42a613da04db6ed23bc6a016c285c0e382ed
Musty260 <51714409+Musty260@users.noreply.github.com>
2021-02-27 15:26:03 +0000

    Add frontend for ContractDisplayPage

---
+++ src/components/Pages/ContractDisplayPage/ContractDisplayPageStyles.tsx
@@ -0,0 +1,16 @@
+import { css } from "emotion";
+
+const Styles = {
+    acceptButton: css`
+        font-family: Quicksand;
+        font-size: 40px;
+        padding: 20px;
+        border-radius: 28px;
+    `,
+
+    informationArea: css`
+        
+    `
+};
+
+export default Styles;
